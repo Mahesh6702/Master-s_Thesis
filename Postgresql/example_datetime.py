52b8cbe11ca209472cc3b36a0183a6023c2110e4
Mahesh Eega <79939325+Mahesh6702@users.noreply.github.com>
2023-02-14 23:10:22 +0100

    Delete Postgresql directory

--- Postgresql/example_datetime.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-# datetime object containing current date and time
-now = datetime.now()
- 
-
-# dd/mm/YY H:M:S
-dt_string = now.strftime(" Date:%d/%m/%Y,Time:%H:%M:%S")
-print(dt_string)
